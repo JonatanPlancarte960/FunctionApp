389460b44f29cc507292734da5ab1ad3f2e6cb1a
JonatanPlancarte960is <jonatanplancarte960is@gmail.com>
2023-08-04 10:49:08 -0600

    Queue Storage

--- HttpExample.cs
+++ HttpExample.cs
@@ -15,6 +15,7 @@ namespace SpaceHTTP
         [FunctionName("HttpExample")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [Queue("outqueue"), StorageAccount("AzureWebJobsStorage")] ICollector<string> msg,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
@@ -24,6 +25,11 @@ namespace SpaceHTTP
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
+
+            if(!string.IsNullOrEmpty(name))
+            {
+                msg.Add(name);
+            }
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
